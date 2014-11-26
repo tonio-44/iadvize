d3fc765a4ef7c74b188d77d27f9398a3422f37d9
Anthony Kébert <atouts-web@orange.fr>
2014-11-26 15:11:50 +0100

    Création du bundle Iadvize/BlogBundle >$ php app/console generate:bundle --namespace=Iadvize/BlogBundle

---
+++ src/Iadvize/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Iadvize\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('IadvizeBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Iadvize/BlogBundle/IadvizeBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Iadvize\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IadvizeBlogBundle extends Bundle
+{
+}
